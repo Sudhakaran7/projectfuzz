6a1384a3698252e9c1651404195348b6145544a1
Sudhakaran7 <sudhakaran1444@gmail.com>
2020-10-04 17:41:54 +0530

    Added

---
+++ assets/js/custom/user-defined.js
@@ -0,0 +1,15 @@
+// User Defined JS
+(function ($) {
+    "use strict";
+
+    //View my Resume Button 1
+    $('#resumeButton-1').on('click', function () {
+        window.open("assets/resume/Arunpandian_Murugan_BE(CSE)_RPA_Profile.pdf");
+    });
+
+    //View my Resume Button 2
+    $('#resumeButton-2').on('click', function () {
+        window.open("assets/resume/Arunpandian_Murugan_BE(CSE)_RPA_Profile.pdf");
+    });
+
+})(jQuery);
